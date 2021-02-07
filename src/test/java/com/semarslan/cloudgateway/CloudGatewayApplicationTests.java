f327aefabdc00254aca16151b0509c3011e1efff
semarslan <sema.arslan@bil.omu.edu.tr>
2021-02-07 10:54:03 +0300

    cloud gateway completed

---
+++ src/test/java/com/semarslan/cloudgateway/CloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semarslan.cloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
